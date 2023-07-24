0ad36cc8b86f8f7c2d0cc6ad475130f00e732580
Kariaki58 <kariakichernocherno@gmail.com>
2023-07-24 16:24:05 +0100

    sorting array using counting sort

---
+++ 102-counting_sort.c
@@ -0,0 +1,36 @@
+#include "sort.h"
+
+void counting_sort(int *array, size_t size)
+{
+	unsigned int status, max = 0, i, dump;
+	int *temp_array, *count_array;
+
+	if (size < 2)
+		return;
+	count_array = malloc(size * sizeof(int));
+	for (status = 0; status < size; status++)
+	{
+		if (array[status] > (int)max)
+			max = array[status];
+		count_array[status] = array[status];
+	}
+	max++;
+	temp_array = malloc(sizeof(int) * (max));
+	if (!temp_array || !count_array)
+		return;
+	/*fill array with 0s*/
+	for (i = 0; i < max; i++)
+		temp_array[i] = 0;
+	for (status = 0; status < size; status++)
+		temp_array[array[status]]++;
+	for (i = 1; i < max; i++)
+		temp_array[i] += temp_array[i - 1];
+	for (status = 0; status < size; status++)
+	{
+		dump = temp_array[count_array[status]] - 1;
+		array[dump] = count_array[status];
+	}
+	print_array(temp_array, max);
+	free(temp_array);
+	free(count_array);
+}
