e664b140beaf093d6ba40f4ceb43a78b94927064
Kariaki58 <kariakichernocherno@gmail.com>
2023-07-26 04:43:04 +0100

    sorts an array of integers

---
+++ 107-quick_sort_hoare.c
@@ -0,0 +1,57 @@
+#include "sort.h"
+
+int find_pivot(int *array, int l, int r, size_t size)
+{
+	int pivot, temp;
+	unsigned int i, j;
+
+	pivot = array[r];
+	i = l - 1;
+	j = r + 1;
+	while (1)
+	{
+		do
+		{
+			i++;
+		}
+		while (array[i] < pivot);
+		do
+		{
+			j--;
+		}
+		while (array[j] > pivot);
+		if (i >= j)
+			return (i);
+		if (i != j)
+		{
+			temp = array[i];
+			array[i] = array[j];
+			array[j] = temp;
+			print_array(array, size);
+		}
+	}
+	return (0);
+}
+
+void call_function(int *array, int l, int r, size_t size)
+{
+	int pivot;
+
+	if (l < r)
+	{
+		pivot = find_pivot(array, l, r, size);
+		call_function(array, l, pivot - 1, size);
+		call_function(array, pivot, r, size);
+	}
+}
+/**
+ * quick_sort_hoare - sorts an array of integers
+ * @array: array of numbers
+ * @size: size of the array
+ */
+void quick_sort_hoare(int *array, size_t size)
+{
+	if (size < 2)
+		return;
+	call_function(array, 0, size - 1, size);
+}
